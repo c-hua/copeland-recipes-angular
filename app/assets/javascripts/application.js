b06621d2ab65e1315ec62dd59770f4a544ddfcbd
Christie Hua <christiehua@gmail.com>
2014-08-02 14:39:04 -0700

    add angular-resource to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require angular/angular
 //= require angular-route/angular-route
 //= require angular-rails-templates
+//= require angular-resource/angular-resource
 //= require_tree .
 
 app.config([ '$routeProvider',
